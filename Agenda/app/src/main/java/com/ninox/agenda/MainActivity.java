af75290bc9215e5f23793a07e523e65c5ac31649
Cesar <cesar.quadros88@gmail.com>
2019-05-08 00:28:51 -0300

    Adicionado activity e redirecionamento

--- Agenda/app/src/main/java/com/ninox/agenda/MainActivity.java
+++ Agenda/app/src/main/java/com/ninox/agenda/MainActivity.java
@@ -2,7 +2,11 @@ package com.ninox.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intentVaiProFormulario = new Intent(MainActivity.this, InicialActivity.class);
+                startActivity(intentVaiProFormulario);
+                Toast.makeText(MainActivity.this, "Usuario Logado", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
