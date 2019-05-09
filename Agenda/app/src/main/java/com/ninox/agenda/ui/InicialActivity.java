d5937db8e4ec162451b04aa154d20314912d9e55
Cesar <cesar.quadros88@gmail.com>
2019-05-08 23:30:20 -0300

    Alteração no layout

--- Agenda/app/src/main/java/com/ninox/agenda/ui/InicialActivity.java
+++ Agenda/app/src/main/java/com/ninox/agenda/ui/InicialActivity.java
@@ -1,8 +1,10 @@
-package com.ninox.agenda;
+package com.ninox.agenda.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ninox.agenda.R;
 
 public class InicialActivity extends AppCompatActivity {
 
--- Agenda/app/src/main/java/com/ninox/agenda/ui/MainActivity.java
+++ Agenda/app/src/main/java/com/ninox/agenda/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ninox.agenda;
+package com.ninox.agenda.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.ninox.agenda.R;
 
 public class MainActivity extends AppCompatActivity {
 
